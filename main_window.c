b9cdd2f96cfcb965fff783612e28805f8028ffad
Your Name <>
2024-12-16 13:24:15 -0600

    Basic main window blueprint

--- main_window.c
+++ main_window.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <ncurses.h>
 
+int timer(int);
+
 int main()
 {
     initscr();
@@ -16,35 +18,76 @@ int main()
     mvaddstr(LINES / 2, (COLS - strlen(title)) / 2, title);
     refresh();
 
+    // Message
+    char* message = NULL;
+
     // Minutes selection
     int minutes = 5; // Default and minimum
     while(1)
     {
-        // Display the value:
+        // Display the chosen value 
         mvprintw(LINES / 2 + 2, (COLS - 5) / 2, "%02d:00", minutes);
 
-        int ch = getch();
-        switch (ch)
+        // Instruction to set message if needed        
+        if (message != NULL)
         {
-        case KEY_LEFT:
-            if (minutes > 5)
-            {
-                minutes--;
-            }
-            break;
-
-        case KEY_RIGHT:
-            if (minutes < 60)
-            {
-                minutes++;
-            }
-        
-        default:
-            // None
-            break;
+            mvaddstr(LINES /2 - 2, (COLS - strlen(message)) / 2, message);
+        }
+        else
+        {
+            // Delete message after 5 seconds
         }
 
+        refresh();
+
+        int ch = getch();
+        if (ch == 27)
+        {
+            break;
+        }
+        else
+        {
+            switch (ch)
+            {
+            case KEY_LEFT:
+                if (minutes > 5)
+                {
+                    minutes--;
+                }
+                break;
+
+            case KEY_RIGHT:
+                if (minutes < 60)
+                {
+                    minutes++;
+                }
+                break;
+
+            case '\n':
+                // Start running app!
+                if (timer(minutes))
+                {
+                    message = "Well done!";
+                }
+                else
+                {
+                    message = "What a shame... better luck next time";
+                }
+                break;
+        
+            default:
+                // None
+                break;
+            }
+        }
     }
+}
 
-
+/*
+This function runs the pomodoro timer given a value in minutes
+*/
+int timer(int minutes)
+{
+    
+    return TRUE;
 }
